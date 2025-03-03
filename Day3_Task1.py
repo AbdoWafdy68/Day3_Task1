21bb94ba770904091ddf1f475c6770bc08354b28
AbdoWafdy68 <abdowafdy81@gmail.com>
2025-03-03 19:58:14 +0200

    Add files via upload

---
+++ Day3_Task1.py
@@ -0,0 +1,11 @@
+name="عبدالرحمن" 
+age="19"
+city="اسوان"
+print("name: عبدالرحمن")
+print("age: 19")
+print("city: اسوان")
+print("hello, my name is"+ name + "and iam" + age + "i live" + city)
+print(type(name))
+print(type(age))
+print(type(city))
+
